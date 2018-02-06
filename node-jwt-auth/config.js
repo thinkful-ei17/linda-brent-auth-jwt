0ec4341951da29983eb712471265f4f382ec188b
Brent Guistwite <brentguistwite@gmail.com>
2018-02-06 14:19:34 -0500

    Previous commit broken. Fix local storage issue

--- node-jwt-auth/config.js
+++ node-jwt-auth/config.js
@@ -6,4 +6,4 @@ exports.DATABASE_URL =
     'mongodb://localhost/jwt-auth-demo';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '15m';
